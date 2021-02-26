c72208cc015df4e6b0320d8a7cc64d22b93d02e1
Takanar <dyganar@hotmail.com>
2021-02-26 09:38:41 -0300

    dia05 - add validações utilizando yup

--- projeto-api/src/controllers/UserController.ts
+++ projeto-api/src/controllers/UserController.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UserRepository } from "../Repositories/UserRepository";
+import * as yup from 'yup';
 
 class UserController {
 
     async create(req:Request, res: Response){
         const { name, email } = req.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é obrigatório"),
+            email: yup.string().email().required("Email incorreto"),
+        })
         
+       // if(!await schema.isValid(req.body)) return res.status(400).json({error: "Validation failed!"});
+        try{
+            await schema.validate(req.body, {abortEarly: false});
+        } catch (err) {
+            return res.status(400).json({ error: err});
+        }
+
         const usersRepository = getCustomRepository(UserRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
